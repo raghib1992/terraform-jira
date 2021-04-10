1d59dfc9a3ee3714a8fdbd23da438b8583960711
raghib1992 <raghib.npti@gmail.com>
2021-04-11 03:28:09 +0530

    done

--- prepare.sh
+++ prepare.sh
@@ -6,7 +6,7 @@ echo "**** Creating new branch*****"
 git checkout $NEW_CUSTOMER
 echo "**** BRANCH FOR $NEW_CUSTOMER CREATED SUCCESSFULLY****"
 
-sed -i 's/newcustomer/$NEW_CUSTOMER/g' ec2/terraform.tfvars
+sed -i 's|newcustomer|$NEW_CUSTOMER|g' /ec2/terraform.tfvars
 git add .
 git commit "change new branch for customer $NEW_CUSTOMER"
 git push origin $NEW_CUSTOMER
