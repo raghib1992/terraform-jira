ee1e1c7eecc668acfe4a55533c47465fe1a8427b
raghib1992 <raghib.npti@gmail.com>
2021-04-11 18:40:15 +0530

    testing

--- ec2/prepare.sh
+++ ec2/prepare.sh
@@ -5,9 +5,3 @@ NEW_CUSTOMER=$1
 git checkout -b $NEW_CUSTOMER
 
 sed -i "s|newcustomer|$NEW_CUSTOMER|g" terraform.tfvars
-
-git add .
-
-git commit -m "change new branch for customer $NEW_CUSTOMER"
-
-git push origin $NEW_CUSTOMER
